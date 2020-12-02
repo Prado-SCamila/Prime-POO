8258a27e514f7158b96bc2ec32d6635dbdf75387
Prado-SCamila <prado.camila.910423@gmail.com>
2020-12-02 00:23:21 -0300

    fim

---
+++ Personagem.cs
@@ -0,0 +1,17 @@
+namespace Prime_POO
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+
+
+        public string Atacar(){
+            return "Ataque de fogo!";
+        }
+        public string Defender(){
+            return "Não foi dessa vez";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Prime_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem = new Personagem();
+            objetoPersonagem.nome = "Tsukamoto";
+            objetoPersonagem.idade = 32;
+            objetoPersonagem.armadura = "Voadora";
+
+            Personagem objetoPersonagem2 = new Personagem();
+            objetoPersonagem2.nome = "Joyce";
+            objetoPersonagem2.idade = 18;
+            objetoPersonagem2.armadura = "Flash";
+
+            Console.WriteLine($"Personagem 1: {objetoPersonagem.nome} - {objetoPersonagem.Atacar()}");
+            Console.WriteLine($"Personagem 1: {objetoPersonagem2.nome} - {objetoPersonagem2.Defender()}");
+        }
+    }}
+
